37431c47c4901dbf3231ffa52dea887067f750b7
Lin Xianyi <iynaix@gmail.com>
2018-02-22 16:49:15 +0800

    Added dynamic selection of league.

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,14 @@ import fetch from "isomorphic-fetch"
 
 const CORS_PROXY_URL = "https://cors-anywhere.herokuapp.com/"
 const POE_NINJA_URL = "http://poe.ninja/api/Data/"
-const LEAGUE = "Abyss"
+
+const getLeague = () => {
+    const bestiary = Date.UTC(2018, 3 - 1, 2, 24 - 4)
+    if (new Date() - bestiary >= 0) {
+        return "Bestiary"
+    }
+    return "Abyss"
+}
 
 export const num = n => {
     if ((n | 0) === n) {
@@ -17,7 +24,7 @@ const fetchNinja = endpoint => {
     const now = new Date()
     const dt = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
 
-    return fetch(`${CORS_PROXY_URL}${POE_NINJA_URL}${endpoint}?league=${LEAGUE}&date=${dt}`, {
+    return fetch(`${CORS_PROXY_URL}${POE_NINJA_URL}${endpoint}?league=${getLeague()}&date=${dt}`, {
         headers: { "X-Requested-With": "XMLHttpRequest" },
     })
         .then(resp => resp.json())
@@ -28,8 +35,12 @@ export const percent = (current, total) => `${num(current / total * 100)}%`
 
 // fetch divination cards
 export const fetchCardRates = () => {
+    return fetchNinja("GetDivinationCardsOverview")
+
+    /*
     if (process.env.NODE_ENV === "production") {
         return fetchNinja("GetDivinationCardsOverview")
     }
     return Promise.resolve(require("./cards.json").lines)
+    */
 }
