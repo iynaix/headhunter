9068ada84983f2ae2b2f47020c3427748a3977dc
Lin Xianyi <iynaix@gmail.com>
2018-11-24 23:59:03 +0800

    Added a loading spinner.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { LEAGUES } from "./constants"
 import { useInput } from "./utils"
 import Headhunter from "./headhunter"
 import Mirror from "./mirror"
+import Spinner from "./spinner"
 import Tabs from "./tabs"
 import headhunterImg from "./Headhunter.png"
 import mirrorImg from "./Mirror.png"
@@ -23,89 +24,49 @@ const CARDS_QUERY = gql`
     }
 `
 
-const cardsById = cards => keyBy(cards, "name")
-
-const CARDS_RESPONSE = {
-    data: {
-        ninjaItems: [
-            {
-                id: 636,
-                name: "House of Mirrors",
-                stackSize: 9,
-                artFilename: "HouseOfMirrors",
-                chaosValue: 3144.75,
-            },
-            {
-                id: 1476,
-                name: "The Doctor",
-                stackSize: 8,
-                artFilename: "TheDoctor",
-                chaosValue: 571.68,
-            },
-            {
-                id: 1496,
-                name: "The Fiend",
-                stackSize: 11,
-                artFilename: "TheFiend",
-                chaosValue: 357.3,
-            },
-            {
-                id: 1529,
-                name: "The Immortal",
-                stackSize: 10,
-                artFilename: "TheImmortal",
-                chaosValue: 303.9,
-            },
-        ],
-    },
-}
-
-const TotalHeader = ({ leagueInput, yourTotalInput, liquidationRatioInput }) => {
-    return (
-        <section className="section">
-            <nav className="level">
-                <div className="level-item has-text-centered" style={{ flexDirection: "column" }}>
-                    <p className="heading is-size-6">League</p>
-                    <div className="select">
-                        <select id="league" name="league" {...leagueInput}>
-                            {LEAGUES.map(league => (
-                                <option key={league} value={league}>
-                                    {league}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
+const OptionHeader = ({ leagueInput, yourTotalInput, liquidationRatioInput }) => (
+    <section className="section">
+        <nav className="level">
+            <div className="level-item has-text-centered" style={{ flexDirection: "column" }}>
+                <p className="heading is-size-6">League</p>
+                <div className="select">
+                    <select id="league" name="league" {...leagueInput}>
+                        {LEAGUES.map(league => (
+                            <option key={league} value={league}>
+                                {league}
+                            </option>
+                        ))}
+                    </select>
                 </div>
-                <div className="level-item has-text-centered">
-                    <div>
-                        <p className="heading is-size-6">Total</p>
-                        <p className="title">
-                            <input className="input" type="text" {...yourTotalInput} />
-                        </p>
-                    </div>
+            </div>
+            <div className="level-item has-text-centered">
+                <div>
+                    <p className="heading is-size-6">Total</p>
+                    <p className="title">
+                        <input className="input" type="text" {...yourTotalInput} />
+                    </p>
                 </div>
-                <div className="level-item has-text-centered">
-                    <div>
-                        <p className="heading is-size-6">Liquidation Ratio</p>
-                        <p className="title">
-                            <input
-                                className="input"
-                                type="number"
-                                {...liquidationRatioInput}
-                                min={0}
-                                max={1}
-                                step={0.1}
-                            />
-                        </p>
-                    </div>
+            </div>
+            <div className="level-item has-text-centered">
+                <div>
+                    <p className="heading is-size-6">Liquidation Ratio</p>
+                    <p className="title">
+                        <input
+                            className="input"
+                            type="number"
+                            {...liquidationRatioInput}
+                            min={0}
+                            max={1}
+                            step={0.1}
+                        />
+                    </p>
                 </div>
-            </nav>
-        </section>
-    )
-}
+            </div>
+        </nav>
+    </section>
+)
 
-const Main = ({ cards }) => {
-    cards = cardsById(cards)
+const Home = () => {
     const leagueInput = useInput("league", LEAGUES[0])
     const yourTotalInput = useInput("yourTotal", 0)
     const liquidationRatioInput = useInput("liquidationRatio", 0.6)
@@ -114,45 +75,53 @@ const Main = ({ cards }) => {
 
     return (
         <div>
-            <TotalHeader
+            <OptionHeader
                 leagueInput={leagueInput}
                 yourTotalInput={yourTotalInput}
                 liquidationRatioInput={liquidationRatioInput}
             />
-            <Tabs
-                titles={[
-                    <img src={headhunterImg} alt="Headhunter" style={{ height: "1.5rem" }} />,
-                    <img src={mirrorImg} alt="Mirror" style={{ height: "1.5rem" }} />,
-                ]}
-            >
-                <Headhunter cards={cards} userTotal={userTotal} />
-                <Mirror cards={cards} userTotal={userTotal} />
-            </Tabs>
+
+            <Query query={CARDS_QUERY}>
+                {({ loading, error, data: { ninjaItems: cards } }) => {
+                    if (loading) {
+                        return (
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                }}
+                            >
+                                <Spinner />
+                            </div>
+                        )
+                    }
+                    if (error) {
+                        console.error(error)
+                        return null
+                    }
+
+                    cards = keyBy(cards, "name")
+
+                    return (
+                        <Tabs
+                            titles={[
+                                <img
+                                    src={headhunterImg}
+                                    alt="Headhunter"
+                                    style={{ height: "1.5rem" }}
+                                />,
+                                <img src={mirrorImg} alt="Mirror" style={{ height: "1.5rem" }} />,
+                            ]}
+                        >
+                            <Headhunter cards={cards} userTotal={userTotal} />
+                            <Mirror cards={cards} userTotal={userTotal} />
+                        </Tabs>
+                    )
+                }}
+            </Query>
         </div>
     )
-}
-
-const Home = () => {
-    /*
-    return (
-        <Query query={CARDS_QUERY}>
-            {({ loading, error, data: { ninjaItems: cards } }) => {
-                if (loading) {
-                    // TODO: loading spinner
-                    return null
-                }
-                if (error) {
-                    console.error(error)
-                    return null
-                }
-
-                return this.renderContent(cards)
-            }}
-        </Query>
-    )
-    */
-
-    return <Main cards={CARDS_RESPONSE.data.ninjaItems} />
 }
 
 export default Home
